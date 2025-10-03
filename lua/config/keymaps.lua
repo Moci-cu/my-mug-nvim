3b9e61b5d5894b307405ce213b881eeb63a3903f
Moci-cu <pekztor4gung@gmail.com>
2025-10-03 15:47:40 +0800

    add terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 local map, opts = vim.keymap.set, { silent = true }
+local terminal = require('config.terminal')
 
 local function clear_search_then_escape()
   if vim.v.hlsearch == 1 then
@@ -12,6 +13,9 @@ map({ 'n', 'v' }, '<C-c>', clear_search_then_escape, { expr = true, desc = 'Esca
 
 map('n', '<leader>f', '<cmd>Telescope find_files<cr>', opts)
 map('n', '<leader>rg', '<cmd>Telescope live_grep<cr>', opts)
+
+map('n', '<leader>ht', terminal.toggle_horizontal, { silent = true, nowait = true, desc = 'Toggle terminal' })
+map('n', '<leader>ts', terminal.toggle_vertical, { silent = true, nowait = true, desc = 'Toggle vertical terminal' })
 
 map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Prev Diagnostic' })
 map('n', ']d', vim.diagnostic.goto_next, { desc = 'Next Diagnostic' })
