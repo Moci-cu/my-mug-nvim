585fdf6953facb699afc66b773f87a1927b580bb
Moci-cu <pekztor4gung@gmail.com>
2025-10-04 08:50:11 +0800

    Remove lazygit plugin and keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,7 +13,6 @@ map({ 'n', 'v' }, '<C-c>', clear_search_then_escape, { expr = true, desc = 'Esca
 
 map('n', '<leader>f', '<cmd>Telescope find_files<cr>', opts)
 map('n', '<leader>rg', '<cmd>Telescope live_grep<cr>', opts)
-map('n', '<leader>lg', '<cmd>LazyGit<cr>', opts)
 
 map('n', '<leader>ht', terminal.toggle_horizontal, { silent = true, nowait = true, desc = 'Toggle terminal' })
 map('n', '<leader>ts', terminal.toggle_vertical, { silent = true, nowait = true, desc = 'Toggle vertical terminal' })
