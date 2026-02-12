bbdbd7cefc8e8b9ef2a1bf15330990187b60c1c4
Moci-cu <pekztor4gung@gmail.com>
2026-02-12 18:26:52 +0800

    update lazy-lock and theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,23 @@
 return {
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      vim.cmd("colorscheme rose-pine")
-    end
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function(_, opts)
+      require("solarized-osaka").setup(opts)
+      vim.cmd("colorscheme solarized-osaka")
+    end,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl, c)
+        hl.NvimTreeNormal = { bg = "none" }
+        hl.NvimTreeNormalNC = { bg = "none" }
+        hl.NvimTreeEndOfBuffer = { bg = "none" }
+      end,
+    },
   }
 }
