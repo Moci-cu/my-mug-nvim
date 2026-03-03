f4641b658e5ae9dd0d423758c21f86d0fcdb0fac
Moci-cu <pekztor4gung@gmail.com>
2026-03-03 14:35:51 +0800

    fix: remap live_grep to <leader>sg to avoid delay with <leader>gr

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,11 +12,11 @@ map({ 'n', 'v' }, '<Esc>', clear_search_then_escape, { expr = true, desc = 'Esca
 map({ 'n', 'v' }, '<C-c>', clear_search_then_escape, { expr = true, desc = 'Escape and clear search' })
 
 map('n', '<leader>f', '<cmd>FzfLua files<cr>', opts)
-map('n', '<leader>rg', '<cmd>FzfLua live_grep<cr>', opts)
+map('n', '<leader>sg', '<cmd>FzfLua live_grep<cr>', opts)
 
 map('n', '<leader>e', '<cmd>Neotree toggle<cr>', { silent = true, desc = 'Toggle Neo-tree' })
 map('n', '<C-h>', '<cmd>Neotree focus<cr>', { silent = true, desc = 'Focus Neo-tree' })
-map('n', '<leader>ht', terminal.toggle_horizontal, { silent = true, nowait = true, desc = 'Toggle terminal' })
+map('n', '<leader>tv', terminal.toggle_horizontal, { silent = true, nowait = true, desc = 'Toggle terminal' })
 map('n', '<leader>ts', terminal.toggle_vertical, { silent = true, nowait = true, desc = 'Toggle vertical terminal' })
 
 map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Prev Diagnostic' })
